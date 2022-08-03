d871b0c4b65eae9cb3f67b5ba64c923d57d71d02
Michael Davis <mcarsondavis@gmail.com>
2022-08-03 13:31:25 -0500

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "A feature-rich, multi-protocol messaging broker";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+    rabbitmq-perf-test = {
+      url = "github:the-mikedavis/rabbitmq-perf-test/flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.utils.follows = "utils";
+    };
+  };
+
+  outputs = { nixpkgs, utils, rabbitmq-perf-test, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (_final: _prev: { inherit (rabbitmq-perf-test.packages.${system}) perf-test; })
+          ];
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            gnumake
+            bazel_5
+            mandoc
+            openjdk
+            openssl
+            python3
+            perf-test
+            (writeShellScriptBin "open" '' exec "${pkgs.xdg-utils}/bin/xdg-open" "$@" '')
+          ];
+          shellHook = ''
+            export CC=${pkgs.clang}/bin/clang
+
+            # This is used by perf-test. I can't tell what sets it in the first place :/
+            unset SIZE
+          '';
+        };
+      });
+}
