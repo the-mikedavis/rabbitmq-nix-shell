96b74e100b4ca5dbeb3dea51a20f9d6ef7a4a649
Michael Davis <mcarsondavis@gmail.com>
2022-08-04 15:16:58 -0500

    Close over the user.bazelrc with a shell script for symlinking

--- flake.nix
+++ flake.nix
@@ -20,19 +20,61 @@
             (_final: _prev: { inherit (rabbitmq-perf-test.packages.${system}) perf-test; })
           ];
         };
+        erlang = pkgs.erlangR25;
+        java = pkgs.openjdk;
+        java-version = builtins.elemAt (builtins.match "([[:digit:]]+).*" java.version) 0;
+        user-bazelrc-text = pkgs.writeText "user.bazelrc" ''
+          build:local --@rules_erlang//:erlang_home=${erlang}/lib/erlang
+          build:local --@rules_erlang//:erlang_version=${erlang.version}
+          build:local --//:elixir_home=${pkgs.elixir_1_13}/lib/elixir
+
+          build --tool_java_language_version=${java-version}
+          build --tool_java_runtime_version=local_jdk
+
+          # rabbitmqctl wait shells out to 'ps', which is broken in the bazel macOS
+          # sandbox (https://github.com/bazelbuild/bazel/issues/7448)
+          # adding "--spawn_strategy=local" to the invocation is a workaround
+          build --spawn_strategy=local
+
+          # --experimental_strict_action_env breaks memory size detection on macOS,
+          # so turn it off for local runs
+          build --noexperimental_strict_action_env
+          build:buildbuddy --experimental_strict_action_env
+
+          # don't re-run flakes automatically on the local machine
+          build --flaky_test_attempts=1
+
+          # Always run locally. Remote builds seem to be broken because of /bin/bash replacements.
+          build --config=local
+
+          # build:buildbuddy --remote_header=x-buildbuddy-api-key=YOUR_API_KEY_HERE
+
+          # cross compile for linux (if on macOS) with rbe
+          # build:rbe --host_cpu=k8
+          # build:rbe --cpu=k8
+        '';
+        linkbazelrc = pkgs.writeShellScriptBin "linkbazelrc" ''
+          read -p "Create $(pwd)/user.bazelrc?" -n 1 -r
+          echo
+          if [[ $REPLY =~ ^[^Nn]$ ]]; then
+            ln -s ${user-bazelrc-text} user.bazelrc
+          fi
+        '';
+        openWrapper = pkgs.writeShellScriptBin "open" ''
+          exec "${pkgs.xdg-utils}/bin/xdg-open" "$@" 
+        '';
       in {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            gnumake
-            bazel_5
-            mandoc
-            openjdk
-            openssl
-            python3
-            perf-test
-            (writeShellScriptBin "open" '' exec "${pkgs.xdg-utils}/bin/xdg-open" "$@" '')
-            erlangR25
-            elixir_1_13
+          packages = [
+            pkgs.gnumake
+            pkgs.bazel_5
+            pkgs.mandoc
+            pkgs.openssl
+            pkgs.python3
+            pkgs.perf-test
+            java
+            linkbazelrc
+            openWrapper
           ];
           shellHook = ''
             export CC=${pkgs.clang}/bin/clang
