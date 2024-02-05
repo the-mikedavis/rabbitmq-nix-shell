a065d2aa6f63a4f2cd81a9d3f716fdd6cf7eb38a
Michael Davis <mcarsondavis@gmail.com>
2024-02-05 17:15:56 -0500

    Minimize flake
    There was a lot in here attempting to make building and running
    Erlang/OTP and/or Rabbit and other tools possible within NixOS. Bazel
    doesn't play nicely with NixOS so I gave up on that in favor of using
    distrobox for any building, even when it's possible to build (e.g. with
    rebar).
    
    So let's minimize the flake for what I use it for in the end: perf-test
    and perf. The `mkFlamegraph` script was cool but I prefer looking at
    flamegraphs in Hotspot now.

--- flake.nix
+++ flake.nix
@@ -4,74 +4,32 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
-    flamegraph-src = {
-      url = "github:brendangregg/FlameGraph";
-      flake = false;
-    };
   };
 
-  outputs = { nixpkgs, utils, flamegraph-src, ... }:
+  outputs = { nixpkgs, utils, ... }:
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        java = pkgs.openjdk;
-        # TODO figure out the graalvm building process...
         perf-test-jar = pkgs.fetchurl {
-          url = "https://github.com/rabbitmq/rabbitmq-perf-test/releases/download/v2.19.0/perf-test-2.19.0.jar";
-          sha256 = "sha256-kgU/JpS5iPHMR2B+6+fzZT2l/Y1X16+RWCYhSnykrNU=";
+          url = "https://github.com/rabbitmq/rabbitmq-perf-test/releases/download/v2.20.0/perf-test-2.20.0.jar";
+          sha256 = "5PIjk0B8RCU+c5a/O+pV+vAyoYvabr5F3/fr/O60UF4=";
         };
         perf-test = pkgs.writeShellScriptBin "perf-test" ''
-          ${java}/bin/java -jar ${perf-test-jar} "$@"
+          ${pkgs.openjdk}/bin/java -jar ${perf-test-jar} "$@"
         '';
-        inherit (pkgs.linuxPackages-libre) perf;
         openWrapper = pkgs.writeShellScriptBin "open" ''
           exec "${pkgs.xdg-utils}/bin/xdg-open" "$@" 
-        '';
-        mkflamegraph = pkgs.writeShellScriptBin "mkflamegraph" ''
-          PERF_DATA_FILE="''${1:-perf.data}"
-          FLAMEGRAPH_FILE="''${2:-flamegraph.svg}"
-          WORK_DIR=`mktemp -d`
-          if [[ ! "$WORK_DIR" || ! -d "$WORK_DIR" ]]; then
-            echo "Could not create temp dir"
-            exit 1
-          fi
-          function cleanup {
-            rm -rf "$WORK_DIR"
-          }
-          trap cleanup EXIT
-
-          ${perf}/bin/perf script --input="$PERF_DATA_FILE" > "$WORK_DIR"/out.perf
-          # Collapse multiline stacks into single lines
-          ${pkgs.perl}/bin/perl ${flamegraph-src + /stackcollapse-perf.pl} "$WORK_DIR"/out.perf > "$WORK_DIR"/out.folded
-          # Merge scheduler profile data
-          ${pkgs.gnused}/bin/sed -e 's/^[0-9]\+_//' "$WORK_DIR"/out.folded > "$WORK_DIR"/out.folded_sched
-          # Create the SVG file
-          ${pkgs.perl}/bin/perl ${flamegraph-src + /flamegraph.pl} --title="CPU Flame Graph" "$WORK_DIR"/out.folded_sched > "$FLAMEGRAPH_FILE"
         '';
       in {
         devShells.default = pkgs.mkShell {
           packages = [
-            pkgs.gnumake
-            pkgs.mandoc
-            pkgs.openssl
-            pkgs.python3
-            java
-            openWrapper
-            # For building OTP by hand:
-            pkgs.ncurses
-            pkgs.libxml2
-            pkgs.libxslt
+            perf-test
             # Kubernetes testing
             pkgs.ytt
             pkgs.kubectl
             (pkgs.google-cloud-sdk.withExtraComponents [pkgs.google-cloud-sdk.components.gke-gcloud-auth-plugin])
-          ] ++ (pkgs.lib.optionals pkgs.stdenv.isLinux [perf mkflamegraph perf-test pkgs.hotspot]);
+          ] ++ (pkgs.lib.optionals pkgs.stdenv.isLinux [openWrapper pkgs.linuxPackages-libre.perf pkgs.hotspot]);
           shellHook = ''
-            export CC=${pkgs.clang}/bin/clang
-
-            # enable profiling support with the JIT on Linux
-            export RABBITMQ_SERVER_ADDITIONAL_ERL_ARGS="+JPperf true"
-
             # This is used by perf-test. I can't tell what sets it in the first place :/
             unset SIZE
           '';
